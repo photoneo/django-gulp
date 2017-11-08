119446bd645944dda13f48dc1773ff5ab610d841
Peter Beno <najlepsiwebdesigner@gmail.com>
2017-11-08 18:11:55 +0100

    fix

--- django_gulp/management/commands/runserver.py
+++ django_gulp/management/commands/runserver.py
@@ -22,19 +22,6 @@ from django.conf import settings
 from env_tools import load_env
 
 
-@staticmethod
-def log_local_message(message_format, *args):
-    """
-    Log a request so that it matches our local log format.
-    """
-    prefix = '{} {}'.format(color('INFO', fg=248), color('request', fg=5))
-    message = message_format % args
-
-    sys.stderr.write('{} {}\n'.format(prefix, message))
-
-basehttp.WSGIRequestHandler.log_message = log_local_message
-
-
 class Command(StaticfilesRunserverCommand):
     """
     Subclass the RunserverCommand from Staticfiles to set up our gulp
